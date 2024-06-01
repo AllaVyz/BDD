e6e6ded28240e101d43b430838eb8368468bd4a8
AllaVyz <efialka@mail.ru>
2024-06-01 13:32:31 +0300

    changed test

---
+++ src/test/java/pages/VerificationPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+
+    private SelenideElement verificationCodeSelector = $("[data-test-id='code'] input");
+    private SelenideElement verificationCodeButton = $("[data-test-id='action-verify']");
+
+    public VerificationPage(){
+        verificationCodeSelector.shouldBe(visible);
+    }
+
+    public DashboardPage activeVerify(DataHelper.VerificationCode code) {
+        verificationCodeSelector.setValue(code.getVerificationCode());
+        verificationCodeButton.click();
+        return new DashboardPage();
+    }
+}
